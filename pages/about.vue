efa9b9310f114bbbc6f70cfbb06751ed78d411d3
shogo-takao <taka.sho2508@gmail.com>
2025-04-18 01:20:49 +0900

    remove pages

--- pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>About Page</h1>
-    <p>This is a basic page demonstrating a generated route</p>
-    <br />
-  </div>
-</template>
--- pages/categories/[category_id]/products.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Products for Category {{ $route.params.category_id }}</h1>
-    <p>This is a more complex route</p>
-    <br />
-  </div>
-</template>
